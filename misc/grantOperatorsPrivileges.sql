7f807bf1df28c699d81e36a0ffb3a864a5aef380
Christian Soelje <csolje@gmail.com>
2018-11-15 14:58:21 +0100

    Fixed the permission

--- misc/grantOperatorsPrivileges.sql
+++ misc/grantOperatorsPrivileges.sql
@@ -6,14 +6,14 @@
 -- v1.0, 2018-10-18 Initial version        Christian Soelje (chso@netcompany.com) Netcompany
 --
 --------------------------------------------------------------------------------------------
-USE [master]
+USE [master];
 GO
 
 CREATE LOGIN [komb-fli-prod\g_komb-flip-sql01_prod_fli_komb_nchosting_dk_admin] FROM WINDOWS WITH DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english];
 GO
-
 GRANT CONTROL SERVER TO [komb-fli-prod\g_komb-flip-sql01_prod_fli_komb_nchosting_dk_admin];
 GO
 USE [DATABASE];
-DENY SELECT on SCHEMA::dbo TO [komb-fli-prod\g_komb-flip-sql01_prod_fli_komb_nchosting_dk_admin];
+CREATE USER [komb-fli-prod\g_komb-flip-sql01_prod_fli_komb_nchosting_dk_admin] FOR LOGIN [komb-fli-prod\g_komb-flip-sql01_prod_fli_komb_nchosting_dk_admin] WITH DEFAULT_SCHEMA=[dbo];
+DENY SELECT TO [komb-fli-prod\g_komb-flip-sql01_prod_fli_komb_nchosting_dk_admin];
 GO
