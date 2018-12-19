32db3f9c5a68e348c9960be40aa68bc482792ccb
Christian Soelje <csolje@gmail.com>
2018-12-19 14:38:27 +0100

    Adding lock monitoring SQL Script

---
+++ misc/getLockingProcess.sql
@@ -0,0 +1,31 @@
+--------------------------------------------------------------------------------------------
+--
+-- Description:
+--  Checking the server for locking processes
+-- Revision history:
+-- v1.0, 2018-12-19 Initial version        Christian Soelje (chso@netcompany.com) Netcompany
+--
+--------------------------------------------------------------------------------------------
+
+
+DECLARE @Table TABLE(
+        SPID INT,
+        Status VARCHAR(MAX),
+        LOGIN VARCHAR(MAX),
+        HostName VARCHAR(MAX),
+        BlkBy VARCHAR(MAX),
+        DBName VARCHAR(MAX),
+        Command VARCHAR(MAX),
+        CPUTime INT,
+        DiskIO INT,
+        LastBatch VARCHAR(MAX),
+        ProgramName VARCHAR(MAX),
+        SPID_1 INT,
+        REQUESTID INT
+)
+
+INSERT INTO @Table EXEC sp_who2
+
+SELECT  COUNT(*)
+FROM    @Table
+WHERE BlkBy >= '.'
