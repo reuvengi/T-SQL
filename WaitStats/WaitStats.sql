8822d8c8129c2f64676f93acf17f0376c6a76fa1
Christian Soelje <csolje@gmail.com>
2019-01-09 10:18:20 +0100

    adding files

---
+++ WaitStats/WaitStats.sql
@@ -0,0 +1,12 @@
+--------------------------------------------------------------------------------------------
+--
+-- Description:
+--  Wait stats
+-- Revision history:
+-- v1.0,  Initial version        Christian Soelje (chso@netcompany.com) Netcompany
+--
+--------------------------------------------------------------------------------------------
+
+SELECT * FROM sys.dm_os_wait_stats WHERE wait_type LIKE 'LCK%' AND wait_time_ms > 0
+
+SELECT resource_type, request_mode, request_type, request_lifetime FROM sys.dm_tran_locks WHERE request_lifetime > 0
