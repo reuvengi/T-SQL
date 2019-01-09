22ae8b4307e9ab671049720a3a6226f3370b4a39
Christian Soelje <csolje@gmail.com>
2019-01-09 10:18:34 +0100

    again

---
+++ Performance/notes.sql
@@ -0,0 +1,41 @@
+
+-- Dynamic management views
+SELECT * FROM sys.dm_exec_sessions AS es
+CROSS APPLY sys.dm_exec_input_buffer(es.session_id, NULL) AS ib
+WHERE es.session_id > 50
+
+-- Longest running query
+SELECT TOP 10
+t.TEXT QueryName,
+s.execution_count AS ExecutionCount,
+s.max_elapsed_time AS MaxElapsedTime,
+ISNULL(s.total_elapsed_time / 1000 / NULLIF(s.execution_count, 0), 0) AS AvgElapsedTime,
+s.creation_time AS LogCreatedOn,
+ISNULL(s.execution_count / 1000 / NULLIF(DATEDIFF(s, s.creation_time, GETDATE()), 0), 0) AS FrequencyPerSec
+,query_plan
+FROM sys.dm_exec_query_stats s
+CROSS APPLY sys.dm_exec_query_plan( s.plan_handle ) u
+CROSS APPLY sys.dm_exec_sql_text( s.plan_handle ) t
+ORDER BY MaxElapsedTime DESC
+
+-- Isolate top waits for server instance since last restart or statistics clear
+WITH Waits AS
+(SELECT wait_type, wait_time_ms / 1000. AS wait_time_s,
+100. * wait_time_ms / SUM(wait_time_ms) OVER() AS pct,
+ROW_NUMBER() OVER(ORDER BY wait_time_ms DESC) AS rn
+FROM sys.dm_os_wait_stats
+WHERE wait_type NOT IN ('CLR_SEMAPHORE','LAZYWRITER_SLEEP','RESOURCE_QUEUE','SLEEP_TASK'
+,'SLEEP_SYSTEMTASK','SQLTRACE_BUFFER_FLUSH','WAITFOR', 'LOGMGR_QUEUE','CHECKPOINT_QUEUE'
+,'REQUEST_FOR_DEADLOCK_SEARCH','XE_TIMER_EVENT','BROKER_TO_FLUSH','BROKER_TASK_STOP','CLR_MANUAL_EVENT'
+,'CLR_AUTO_EVENT','DISPATCHER_QUEUE_SEMAPHORE', 'FT_IFTS_SCHEDULER_IDLE_WAIT'
+,'XE_DISPATCHER_WAIT', 'XE_DISPATCHER_JOIN', 'SQLTRACE_INCREMENTAL_FLUSH_SLEEP'))
+SELECT W1.wait_type,
+CAST(W1.wait_time_s AS DECIMAL(12, 2)) AS wait_time_s,
+CAST(W1.pct AS DECIMAL(12, 2)) AS pct,
+CAST(SUM(W2.pct) AS DECIMAL(12, 2)) AS running_pct
+FROM Waits AS W1
+INNER JOIN Waits AS W2
+ON W2.rn <= W1.rn
+GROUP BY W1.rn, W1.wait_type, W1.wait_time_s, W1.pct
+HAVING SUM(W2.pct) - W1.pct < 99 OPTION (RECOMPILE); -- percentage threshold
+GO
