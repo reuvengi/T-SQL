bd4301aee2d89658b4882f57e08d17c3884819c3
Christian Soelje <csolje@gmail.com>
2019-02-11 11:12:30 +0100

    Modified checkLogFile.sql

--- misc/checkLogFile.sql
+++ misc/checkLogFile.sql
@@ -1,3 +1,12 @@
+--------------------------------------------------------------------------------------------
+--
+-- Description:
+--  Finding the log files used space and free space
+-- Revision history:
+-- v1.0, 2019-01-29 Initial version        Christian Soelje (chso@netcompany.com) Netcompany
+--
+--------------------------------------------------------------------------------------------
+
 SELECT name AS [File Name], 
         physical_name AS [Physical Name], 
         size/128.0 AS [Total Size in MB], 
