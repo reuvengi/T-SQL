5fc38bbb46ca67fec08bc6440d42e80f5dd9e5a7
Christian Solje <csolje@gmail.com>
2019-06-04 18:05:05 +0200

    Added getDatabasesMissingBackupPast24Hours.sql

---
+++ Backup_Restore/getDatabasesMissingBackupPast24Hours.sql
@@ -0,0 +1,37 @@
+--------------------------------------------------------------------------------------------
+--
+-- Description:
+--  Getting a list of databases missing a full backup within the past 24 hours
+--
+-- Revision history:
+-- v1.0, 2019-06-04 Initial version        Christian Solje (csolje@gmail.com) @dbamist
+--
+--------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------- 
+--Databases Missing a Data (aka Full) Back-Up Within Past 24 Hours 
+------------------------------------------------------------------------------------------- 
+--Databases with data backup over 24 hours old 
+SELECT 
+   CONVERT(CHAR(100), SERVERPROPERTY('Servername')) AS Server, 
+   msdb.dbo.backupset.database_name, 
+   MAX(msdb.dbo.backupset.backup_finish_date) AS last_db_backup_date, 
+   DATEDIFF(hh, MAX(msdb.dbo.backupset.backup_finish_date), GETDATE()) AS [Backup Age (Hours)] 
+FROM    msdb.dbo.backupset 
+WHERE     msdb.dbo.backupset.type = 'D'  
+GROUP BY msdb.dbo.backupset.database_name 
+HAVING      (MAX(msdb.dbo.backupset.backup_finish_date) < DATEADD(hh, - 24, GETDATE()))  
+
+UNION  
+
+--Databases without any backup history 
+SELECT      
+   CONVERT(CHAR(100), SERVERPROPERTY('Servername')) AS Server,  
+   master.dbo.sysdatabases.NAME AS database_name,  
+   NULL AS [Last Data Backup Date],  
+   9999 AS [Backup Age (Hours)]  
+FROM 
+   master.dbo.sysdatabases LEFT JOIN msdb.dbo.backupset 
+       ON master.dbo.sysdatabases.name  = msdb.dbo.backupset.database_name 
+WHERE msdb.dbo.backupset.database_name IS NULL AND master.dbo.sysdatabases.name <> 'tempdb' 
+ORDER BY  
+   msdb.dbo.backupset.database_name 
