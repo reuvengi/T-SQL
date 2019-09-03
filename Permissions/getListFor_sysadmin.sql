b816d45a8bb68c40fb591026274cfff12a72e524
Christian Solje <csolje@gmail.com>
2019-09-03 16:10:19 +0200

    Updated getting more details

--- Permissions/getListFor_sysadmin.sql
+++ Permissions/getListFor_sysadmin.sql
@@ -7,4 +7,9 @@
 -- v1.0, 2019-09-03 Initial version        Christian Solje (csolje@gmail.com) @dbamist
 --
 --------------------------------------------------------------------------------------------
-SELECT name, type_desc, is_disabled FROM master.sys.server_principals WHERE IS_SRVROLEMEMBER('sysadmin',name) = 1 ORDER BY name
+SELECT 'Name' = sp.NAME
+    ,sp.is_disabled AS [Is_disabled]
+FROM sys.server_role_members rm
+    ,sys.server_principals sp
+WHERE rm.role_principal_id = SUSER_ID('Sysadmin')
+    AND rm.member_principal_id = sp.principal_id
