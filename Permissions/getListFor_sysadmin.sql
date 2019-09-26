35a7d66e262992565c496ba221d890df3b3eeb9e
Christian Solje <christian.solje@sentia.dk>
2019-09-26 17:24:36 +0200

    Updated getListFor_sysadmins.sql

--- Permissions/getListFor_sysadmin.sql
+++ Permissions/getListFor_sysadmin.sql
@@ -7,9 +7,22 @@
 -- v1.0, 2019-09-03 Initial version        Christian Solje (csolje@gmail.com) @dbamist
 --
 --------------------------------------------------------------------------------------------
-SELECT 'Name' = sp.NAME
-    ,sp.is_disabled AS [Is_disabled]
-FROM sys.server_role_members rm
-    ,sys.server_principals sp
-WHERE rm.role_principal_id = SUSER_ID('Sysadmin')
-    AND rm.member_principal_id = sp.principal_id
+USE master
+GO
+SELECT DISTINCT p.name AS [loginname] ,
+p.type ,
+p.type_desc ,
+p.is_disabled,
+s.sysadmin,
+CONVERT(VARCHAR(10),p.create_date ,101) AS [created],
+CONVERT(VARCHAR(10),p.modify_date , 101) AS [update]
+FROM sys.server_principals p
+JOIN sys.syslogins s ON p.sid = s.sid
+JOIN sys.server_permissions sp ON p.principal_id = sp.grantee_principal_id
+WHERE p.type_desc IN ('SQL_LOGIN', 'WINDOWS_LOGIN', 'WINDOWS_GROUP')
+-- Logins that are not process logins
+AND p.name NOT LIKE '##%'
+-- Logins that are sysadmins or have GRANT CONTROL SERVER
+AND (s.sysadmin = 1 OR sp.permission_name = 'CONTROL SERVER')
+ORDER BY p.name
+GO
