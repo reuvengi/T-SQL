b7bd49e25e7ddd2197f7959ccc11d9e812d68edd
Christian Solje <christian.solje@aller.com>
2020-12-04 22:57:53 +0100

    Updated CPUConfigurationDetails

--- NC/CPUConfigurationDetails.sql
+++ NC/CPUConfigurationDetails.sql
@@ -3,6 +3,6 @@
 SELECT cpu_count AS [Logical CPU Count] ,
 hyperthread_ratio AS [Hyperthread Ratio] ,
 cpu_count / hyperthread_ratio AS [Physical CPU Count] ,
-physical_memory_in_bytes / 1048576 AS [Physical Memory (MB)] ,
+physical_memory_kb / 1048576 AS [Physical Memory (MB)] ,
 sqlserver_start_time
 FROM sys.dm_os_sys_info ;
